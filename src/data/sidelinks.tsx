1a07a3c3f95dad730d765dda0d8b693b6b08044a
Burhanuddin Rampura <burhanuddin.devstree@gmail.com>
2024-09-24 17:03:31 +0530

    first commit

---
+++ src/data/sidelinks.tsx
@@ -0,0 +1,84 @@
+import {
+  IconComponents,
+  IconHexagonNumber1,
+  IconHexagonNumber2,
+  IconHexagonNumber3,
+  IconHexagonNumber4,
+  IconHexagonNumber5,
+  IconLayoutDashboard,
+  IconSettings,
+  IconUserShield,
+} from '@tabler/icons-react'
+
+export interface NavLink {
+  title: string
+  label?: string
+  href: string
+  icon: JSX.Element
+}
+
+export interface SideLink extends NavLink {
+  sub?: NavLink[]
+}
+
+export const sidelinks: SideLink[] = [
+  {
+    title: 'Dashboard',
+    label: '',
+    href: '/',
+    icon: <IconLayoutDashboard size={18} />,
+  },
+
+  {
+    title: 'Authentication',
+    label: '',
+    href: '',
+    icon: <IconUserShield size={18} />,
+    sub: [
+      {
+        title: 'Sign In (email + password)',
+        label: '',
+        href: '/sign-in',
+        icon: <IconHexagonNumber1 size={18} />,
+      },
+      {
+        title: 'Sign In (Box)',
+        label: '',
+        href: '/sign-in-2',
+        icon: <IconHexagonNumber2 size={18} />,
+      },
+      {
+        title: 'Sign Up',
+        label: '',
+        href: '/sign-up',
+        icon: <IconHexagonNumber3 size={18} />,
+      },
+      {
+        title: 'Forgot Password',
+        label: '',
+        href: '/forgot-password',
+        icon: <IconHexagonNumber4 size={18} />,
+      },
+      {
+        title: 'OTP',
+        label: '',
+        href: '/otp',
+        icon: <IconHexagonNumber5 size={18} />,
+      },
+    ],
+  },
+
+  {
+    title: 'Extra Components',
+    label: '',
+    href: '/extra-components',
+    icon: <IconComponents size={18} />,
+  },
+
+  {
+    title: 'Settings',
+    label: '',
+    href: '/settings',
+    icon: <IconSettings size={18} />,
+  },
+]
